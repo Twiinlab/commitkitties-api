7c0c479bb26c19ff8fec5c3c8353d5b53b2b9f13
ruben chavarri <ruben.chavarri.contractor@bbva.com>
2018-11-01 13:00:57 +0100

    update listener events

--- src/listener/index.js
+++ src/listener/index.js
@@ -1,30 +1,73 @@
 'use strict';
 
 import * as contracts from '../utils/contracts.js';
+import * as kittyService from '../routes/kitties/service';
+
 import config  from '../../config';
 
-function printEvent( event, returnValues, blockNumber ){
-    console.log('----------------------------------------------');
-    console.log(`--------- EVENT (${event}) START ---------`)
-    console.log(`returnValues = ${returnValues}`)
-    console.log(`blockNumber (${blockNumber})`)
-}
 
 module.exports.watchContract = async () => {
-    const contract = await contracts.getContract('KittyCore');
+    watchAllEvents( await contracts.getContract('KittyCore') );
+    watchAllEvents( await contracts.getContract('SaleClockAuction') );
+}
+
+function printEvent( log ){
+    console.log('----------------------------------------------');
+    console.log(`--------- EVENT (${log.event}) START ---------`)
+    console.log(`returnValues = ${JSON.stringify(log.returnValues)}`)
+    console.log(`blockNumber (${log.blockNumber})`)
+}
+
+async function watchAllEvents( contract ) {
     try {
         contract.events.allEvents({},  function(error, event){ 
             if (error) {
               console.log(`allEvents Error: ${error}`) 
             }
           })
-          .on('data', (log) => {
-            printEvent( log.event, log.returnValues, log.blockNumber )
+          .on('data', async (log) => {
+            printEvent( log )
+            try {
+              let kitty;
+              switch (log.event) {
+                case 'Transfer':
+                  kitty = await kittyService.getKittyById(log.returnValues.tokenId);
+                  kitty.data.owner = { address: log.returnValues.to };
+                  await kittyService.updateKitty(kitty.id, kitty.data);
+                  //log.returnValues="from":"0x4AAa4E3ce8E9d8A6533B75db54da017E2Cf811c8","to":"0x2B7ec3747C6d4EBfd79512Ac2ad32C189aFe911c","tokenId":"12"
+                  break;
+                case 'AuctionCreated':
+                  //log.returnValues="tokenId":"12","startingPrice":"200","endingPrice":"200","duration":"200"
+                  kitty = await kittyService.getKittyById(log.returnValues.tokenId);
+                  kitty.data.auction = { price: log.returnValues.startingPrice };
+                  await kittyService.updateKitty(kitty.id, kitty.data);
+                  break;
+                case 'AuctionSuccessful':
+                  //log.returnValues="tokenId":"13","totalPrice":"100","winner":"0x6999e1D9ec10d0b0D06C657e289F55A2e17DEa64"
+                  kitty = await kittyService.getKittyById(log.returnValues.tokenId);
+                  kitty.data.auction = { };
+                  kitty.data.owner = { address: log.returnValues.winner };
+                  await kittyService.updateKitty(kitty.id, kitty.data);
+                  break;
+                case 'AuctionCancelled':
+                  //log.returnValues="tokenId":"13",
+                  kitty = await kittyService.getKittyById(log.returnValues.tokenId);
+                  kitty.data.auction = { };
+                  await kittyService.updateKitty(kitty.id, kitty.data);
+                  break;
+                default:
+                  break;
+              }
+            } catch (error) {
+                console.log(`watchAllEvents Event: ${log.event} Error: ${error}`)
+            }
+            
             //CreateAuction
             //Birth
             //Bid
             //CancelAuction
-  
+            
+
           })
           .on('changed', (log) => {
               console.log(`Changed: ${log}`)
@@ -36,4 +79,5 @@ module.exports.watchContract = async () => {
     } catch (error) {
         console.log(`Event Listener error: ${error}`);
     }
-  }
+}
+
