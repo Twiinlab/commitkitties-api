db3b341b31bea6cf2fad84ac5ea8d7d6c0e89378
ruben chavarri <ruben.chavarri.contractor@bbva.com>
2018-11-15 23:15:44 +0100

    update ganache ip

--- scripts/console-queries.sh
+++ scripts/console-queries.sh
@@ -1,5 +1,6 @@
 
 web3.eth.getBalance('0xE370c2C187193ccF3B9265917C557602F68aC11f', function(err,res) { console.log(res.toNumber()) })
+web3.eth.getBalance('0x4aaa4e3ce8e9d8a6533b75db54da017e2cf811c8', function(err,res) { console.log(res.toNumber()) })
 
 DEPLOY GCLOUD
 =============
